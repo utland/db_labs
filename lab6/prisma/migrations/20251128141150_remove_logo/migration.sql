0c51109bd7855c9fe918723a8c41c1076d15ab38
utland <sizonenkoilya0@gmail.com>
2025-11-28 16:12:59 +0200

    migrate remove-logo

---
+++ lab6/prisma/migrations/20251128141150_remove_logo/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `logo` on the `supplier` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "supplier" DROP COLUMN "logo";
